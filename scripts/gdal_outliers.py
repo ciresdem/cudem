4350ea85718f6ca33ff44922728e59aabbbe4d4f
matthew love <matthew.love@colorado.edu>
2021-03-26 15:43:52 -0700

    gdal_outliers help

--- scripts/gdal_outliers.py
+++ scripts/gdal_outliers.py
@@ -4,13 +4,51 @@ import sys
 from cudem import demfun
 from cudem import utils
 
+_version = '0.0.1'
+_usage = '''gdal_outliers.py ({}): filter z outliers in a DEM
+
+usage: gdal_outliers.py [ file ]
+
+ Options:
+  file\t\tThe input DEM file-name
+
+  --help\tPrint the usage text
+  --version\tPrint the version information
+
+ Examples:
+ % gdal_outliers.py input.tif
+
+CIRES DEM home page: <http://ciresgroups.colorado.edu/coastalDEM>
+'''.format(_version)
+
 if __name__ == "__main__":
-    src_gdal = sys.argv[1]
-    dst_gdal = src_gdal.split('.')[0] + '_fltr.tif'
+
+    elev = None
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg == '-help' or arg == '--help' or arg == '-h':
+            sys.stderr.write(_usage)
+            sys.exit(1)
+        elif arg == '-version' or arg == '--version':
+            sys.stderr.write('{}\n'.format(_version))
+            sys.exit(1)
+        elif elev is None: elev = arg
+        else:
+            sys.stderr.write(_usage)
+            sys.exit(1)
+        i = i + 1
+
+    if elev is None:
+        sys.stderr.write(_usage)
+        utils.echo_error_msg('you must enter an input file')
+        sys.exit(1)
+  
+    dst_gdal = elev.split('.')[0] + '_fltr.tif'
     chunk_size = 10
     chunk_step = chunk_size
     threshhold = 5
-    utils.echo_msg('filtering {} to {}'.format(src_gdal, dst_gdal))
-    demfun.filter_outliers(src_gdal, dst_gdal, threshhold=threshhold, chunk_size=chunk_size, chunk_step=chunk_step)
+    utils.echo_msg('filtering {} to {}'.format(elev, dst_gdal))
+    demfun.filter_outliers(elev, dst_gdal, threshhold=threshhold, chunk_size=chunk_size, chunk_step=chunk_step)
 
 ### End
