540916a308557ab37cf8d376c035d74bcbce269a
matthew love <matthew.love@colorado.edu>
2021-06-18 09:32:16 -0700

    spatial metadata extend

--- cudem/metadata.py
+++ cudem/metadata.py
@@ -269,13 +269,12 @@ usage: spatial_metadata.py [ datalist [ OPTIONS ] ]
   -O, --output-name\tBASENAME for all outputs.
   -P, --epsg\t\tHorizontal projection of data as EPSG code [4326]
   -X, --extend\t\tNumber of cells with which to EXTEND the REGION.
-\t\t\tappend :<num> to extend the processing region: -X6:12
 
   -p, --prefix\t\tSet BASENAME (-O) to PREFIX (append inc/region/year info to output BASENAME).
   -r, --grid-node\tUse grid-node registration, default is pixel-node
   -q, --quiet\t\tLower verbosity to a quiet. (overrides --verbose)
-  --help\tPrint the usage text
-  --version\tPrint the version information
+  --help\t\tPrint the usage text
+  --version\t\tPrint the version information
 
  Examples:
   % spatial_metadata.py my_data.datalist
@@ -318,10 +317,17 @@ def spat_meta_cli(argv = sys.argv):
         elif arg[:2] == '-E':
             inc = utils.str2inc(arg[2:])
         elif arg == '--extend' or arg == '-X':
-            extend = utils.int_or(argv[i + 1], 0)
-            i = i + 1
+            exts = argv[i + 1].split(':')
+            extend = utils.int_or(exts[0], 0)
+            i += 1
         elif arg[:2] == '-X':
-            extend = utils.int_or(arg[2:], 0)
+            exts = arg[2:].split(':')
+            extend = utils.int_or(exts[0], 0)
+        # elif arg == '--extend' or arg == '-X':
+        #     extend = utils.int_or(argv[i + 1], 0)
+        #     i = i + 1
+        # elif arg[:2] == '-X':
+        #     extend = utils.int_or(arg[2:], 0)
         elif arg == '-r' or arg == '--grid-node': node = 'grid'
         elif arg == '-p' or arg == '--prefix': want_prefix = True
         elif arg == '-v' or arg == '--validate': want_valid = True
