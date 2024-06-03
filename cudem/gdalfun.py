b01f1aa2cebd46f447365ff1cb3ec522da71bc95
Matthew Love <matthew.love@colorado.edu>
2024-06-03 10:54:50 -0700

    gdalwarp

--- cudem/gdalfun.py
+++ cudem/gdalfun.py
@@ -1388,11 +1388,18 @@ def sample_warp(
         pbar_update = None
         
     #with tqdm(desc='warping...', total=100) as pbar:
-    dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, format='MEM' if dst_dem is None else 'GTiff',
-                       xRes=x_sample_inc, yRes=y_sample_inc, targetAlignedPixels=tap, #width=xcount, height=ycount,
-                       dstNodata=ndv, outputBounds=out_region, outputBoundsSRS=dst_srs if out_region is not None else None,
-                       resampleAlg=sample_alg, errorThreshold=0, #options=["COMPRESS=LZW", "TILED=YES"],
-                       srcSRS=src_srs, dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
+    if (xcount is None or ycount is None):
+        dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, format='MEM' if dst_dem is None else 'GTiff',
+                           xRes=x_sample_inc, yRes=y_sample_inc, targetAlignedPixels=tap, #width=xcount, height=ycount,
+                           dstNodata=ndv, outputBounds=out_region, outputBoundsSRS=dst_srs if out_region is not None else None,
+                           resampleAlg=sample_alg, errorThreshold=0, #options=["COMPRESS=LZW", "TILED=YES"],
+                           srcSRS=src_srs, dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
+    else:
+        dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, format='MEM' if dst_dem is None else 'GTiff',
+                           targetAlignedPixels=tap, width=xcount, height=ycount,
+                           dstNodata=ndv, outputBounds=out_region, outputBoundsSRS=dst_srs if out_region is not None else None,
+                           resampleAlg=sample_alg, errorThreshold=0, #options=["COMPRESS=LZW", "TILED=YES"],
+                           srcSRS=src_srs, dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
 
     if verbose:
         pbar.close()
