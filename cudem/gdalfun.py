5350b5a7b5314fea02847070276e68a8de0983ac
Matthew Love <matthew.love@colorado.edu>
2024-06-03 10:12:06 -0700

    test

--- cudem/gdalfun.py
+++ cudem/gdalfun.py
@@ -1373,7 +1373,7 @@ def sample_warp(
     #         )
     #     )        
     utils.echo_msg('gdalwarp -s_srs {} -t_srs {} -tr {} {} -r {}'.format(src_srs, dst_srs, x_sample_inc, y_sample_inc, sample_alg))
-    utils.echo_msg(dst_dem)
+    utils.echo_msg('dst_dem: > {} <'.format(dst_dem))
     if dst_dem is not None:
         if not os.path.exists(os.path.dirname(dst_dem)):
             os.makedirs(os.path.dirname(dst_dem))
