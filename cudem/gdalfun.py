a1da0ee6a5b18bd8d7030497f7c3cc01d3eb96f2
Matthew Love <matthew.love@colorado.edu>
2024-06-03 08:44:59 -0700

    test

--- cudem/gdalfun.py
+++ cudem/gdalfun.py
@@ -1391,7 +1391,7 @@ def sample_warp(
                        xRes=x_sample_inc, yRes=y_sample_inc, targetAlignedPixels=tap, width=xcount, height=ycount,
                        dstNodata=ndv, outputBounds=out_region, outputBoundsSRS=dst_srs if out_region is not None else None,
                        resampleAlg=sample_alg, errorThreshold=0, options=["COMPRESS=LZW", "TILED=YES"],
-                       srcSRS=src_srs, dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
+                       dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
 
     if verbose:
         pbar.close()
