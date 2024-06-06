4879955ba45d3c465513cd99b4077f47d73df5c6
Matthew Love <matthew.love@colorado.edu>
2024-06-06 16:44:02 -0700

    deflate warp

--- cudem/gdalfun.py
+++ cudem/gdalfun.py
@@ -1384,10 +1384,10 @@ def sample_warp(
     warp_options = gdal.WarpOptions(format='MEM' if dst_dem is None else 'GTiff',
                                     xRes=x_sample_inc, yRes=y_sample_inc, targetAlignedPixels=tap, width=xcount, height=ycount,
                                     dstNodata=ndv, outputBounds=out_region, outputBoundsSRS=dst_srs if out_region is not None else None,
-                                    resampleAlg=sample_alg, errorThreshold=0, options=["COMPRESS=DEFLATE", "TILED=YES"],
+                                    resampleAlg=sample_alg, errorThreshold=0, creationOptions=["COMPRESS=DEFLATE", "TILED=YES"],
                                     srcSRS=src_srs, dstSRS=dst_srs, outputType=gdal.GDT_Float32, callback=pbar_update)
     
-    dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, options=warp_options)
+    dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, warpOptions=warp_options)
     
     # if (xcount is None or ycount is None):
     #     dst_ds = gdal.Warp('' if dst_dem is None else dst_dem, src_dem, format='MEM' if dst_dem is None else 'GTiff',
